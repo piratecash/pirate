08141588979a668b750a28c53188b9ff305d5747
MarcoFalke <falke.marco@gmail.com>
2017-08-16 00:35:19 +0200

    Merge #10956: Fix typos
    9d5e98ff8 Fix typos. (practicalswift)
    
    Pull request description:
    
      Fix some typos not covered by #10705.
    
    Tree-SHA512: f06e9541f6ae13ef5d6731399b61795997b21a8816abeb1749c93e99a5c47354e6cbd4a3d145f4dc6ef8a13db179799a3121ecbb7288abf3e8d81cdf81500d37

--- test/functional/rawtransactions.py
+++ test/functional/rawtransactions.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2020-2022 The Cosanta Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test the rawtranscation RPCs.
+"""Test the rawtransaction RPCs.
 
 Test the following RPCs:
    - createrawtransaction
