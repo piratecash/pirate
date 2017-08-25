0cd4410baced26426bc400dc32281c1a7b93c9e2
MarcoFalke <falke.marco@gmail.com>
2017-08-24 20:59:06 -0400

    Merge #11112: [developer-notes] By default, declare single-argument constructors "explicit"
    f1708ef89 Add recommendation: By default, declare single-argument constructors `explicit` (practicalswift)
    
    Pull request description:
    
      This is a follow-up to the now merged #10969.
    
      Add recommendation:
    
      > By default, declare single-argument constructors `explicit`.
      >
      > - *Rationale*: This is a precaution to avoid unintended conversions that might arise when single-argument constructors are used as implicit conversion functions.
      >
    
    Tree-SHA512: 1ceb1008a7863ebd0f09ba9c06b4e28b3b03265d7381f9d0c8bd4be1663d5d0392de0ecd811027aa27c0d962723674b245b3c165a437942a776f3525db39d36b

--- src/test/addrman_tests.cpp
+++ src/test/addrman_tests.cpp
@@ -16,7 +16,7 @@ class CAddrManTest : public CAddrMan
     uint64_t state;
 
 public:
-    CAddrManTest(bool makeDeterministic = true)
+    explicit CAddrManTest(bool makeDeterministic = true)
     {
         state = 1;
 
