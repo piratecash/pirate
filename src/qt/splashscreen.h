6187e903f6228c3e7c00fe8105214ac2892630fc
Jonas Schnelli <dev@jonasschnelli.ch>
2017-09-07 09:20:55 -0700

    Merge #10770: Drop upgrade-cancel callback registration for a generic "cancelable"
    ee4d1493e Drop upgrade-cancel callback registration for a generic "resumeable" (Matt Corallo)
    
    Pull request description:
    
      Instead of passing a StartShutdown reference all the way up from
      txdb, give ShowProgress a "cancelable" boolean, as StartShutdown
      is pretty much always what you'll want to use to cancel. Use the
      same boolean to allow cancel during initial block verification.
    
    Tree-SHA512: 515817aaa4b9e3e856200e00be9c2d44ecfa2d4f288fe3e02116105fe85de2650c13076ee7e45396ec1ce6ab45e53b0477cddda7cfdee5b3bd0589cb81a4c346

--- src/qt/splashscreen.h
+++ src/qt/splashscreen.h
@@ -37,8 +37,6 @@ public Q_SLOTS:
     /** Show message and progress */
     void showMessage(const QString &message, int alignment, const QColor &color);
 
-    /** Sets the break action */
-    void setBreakAction(const std::function<void(void)> &action);
 protected:
     bool eventFilter(QObject * obj, QEvent * ev);
 
@@ -56,8 +54,6 @@ private:
     int curAlignment;
 
     QList<CWallet*> connectedWallets;
-
-    std::function<void(void)> breakAction;	
 };
 
 #endif // BITCOIN_QT_SPLASHSCREEN_H
