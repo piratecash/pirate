4657e2f62518729a1ab2d78ed4aeb10779304a7a
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-22 11:04:37 +0100

    Merge #12076: qa: Use node.datadir instead of tmpdir in test framework
    c8330d4 qa: Use node.datadir instead of tmpdir in test framework (MarcoFalke)
    
    Pull request description:
    
      Commit c53c9831eedaf3b311bb942945268830f9ba3abc introduced the utility function `get_datadir_path`, however not all places in the code use this util function. Using the util function everywhere makes it easier to review pull requests related to the datadir.
    
      This commit replaces datadir path creation with the `datadir` member of `TestNode`, which itself uses `get_datadir_path`.
    
    Tree-SHA512: c75707ab7149d732a6d56152a5813138a33459d3d07577b60b89f2a207c83b7663fac5f203593677c9892d1c23a5eba4bd45c5c4ababf040d720b437240fcddf

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -299,7 +299,7 @@ def rpc_url(datadir, i, rpchost=None):
 ################
 
 def initialize_datadir(dirname, n):
-    datadir = os.path.join(dirname, "node" + str(n))
+    datadir = get_datadir_path(dirname, n)
     if not os.path.isdir(datadir):
         os.makedirs(datadir)
     with open(os.path.join(datadir, "cosanta.conf"), 'w', encoding='utf8') as f:
@@ -313,9 +313,8 @@ def initialize_datadir(dirname, n):
 def get_datadir_path(dirname, n):
     return os.path.join(dirname, "node" + str(n))
 
-def append_config(dirname, n, options):
-    datadir = get_datadir_path(dirname, n)
-    with open(os.path.join(datadir, "bitcoin.conf"), 'a', encoding='utf8') as f:
+def append_config(datadir, options):
+    with open(os.path.join(datadir, "cosanta.conf"), 'a', encoding='utf8') as f:
         for option in options:
             f.write(option + "\n")
 
