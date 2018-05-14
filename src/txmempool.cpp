e3e64baf53e8adff35e6fa86ce9e8d2e3297e4c0
MarcoFalke <falke.marco@gmail.com>
2018-05-14 10:29:17 -0400

    Merge #11689: mempool: Fix missing locking in CTxMemPool::check(…) and CTxMemPool::setSanityCheck(…)
    47782b49e6 Add Clang thread safety analysis annotations (practicalswift)
    0e2dfa8a65 Fix missing locking in CTxMemPool::setSanityCheck(double dFrequency) (practicalswift)
    6bc5b7100b Fix missing locking in CTxMemPool::check(const CCoinsViewCache *pcoins) (practicalswift)
    
    Pull request description:
    
      Fix missing locking in `CTxMemPool::check(const CCoinsViewCache *pcoins)`:
      * reading variable `mapTx` requires holding mutex `cs`
      * reading variable `mapNextTx` requires holding mutex `cs`
      * reading variable `nCheckFrequency` requires holding mutex `cs`
    
      Fix missing locking in `CTxMemPool::setSanityCheck(double dFrequency)`:
      * writing variable `nCheckFrequency` requires holding mutex `cs`
    
    Tree-SHA512: ce7c365ac89225223fb06e6f469451b121acaa499f35b21ad8a6d2a266c91194639b3703c5428871be033d4f5f7be790cc297bd8c25b2e0c59345ef09c3693d0

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -1017,6 +1017,7 @@ static void CheckInputsAndUpdateCoins(const CTransaction& tx, CCoinsViewCache& m
 
 void CTxMemPool::check(const CCoinsViewCache *pcoins) const
 {
+    LOCK(cs);
     if (nCheckFrequency == 0)
         return;
 
@@ -1031,7 +1032,6 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const
     CCoinsViewCache mempoolDuplicate(const_cast<CCoinsViewCache*>(pcoins));
     const int64_t spendheight = GetSpendHeight(mempoolDuplicate);
 
-    LOCK(cs);
     std::list<const CTxMemPoolEntry*> waitingOnDependants;
     for (indexed_transaction_set::const_iterator it = mapTx.begin(); it != mapTx.end(); it++) {
         unsigned int i = 0;
