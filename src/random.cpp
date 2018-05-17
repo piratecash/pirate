c192b2cd8f53402101f2539f0cbbff45d8b03328
MarcoFalke <falke.marco@gmail.com>
2018-05-17 12:25:09 -0400

    Merge #13236: break circular dependency: random/sync -> util -> random/sync
    84f41946b9 break circular dependency: random/sync -> util -> random/sync (Chun Kuan Lee)
    
    Pull request description:
    
      LogPrintf  has acutally been moved to logging.h
    
    Tree-SHA512: a7135f5fea421e62f010f2e434873bd1c1738f115453377dada7d24900b3b095535d8aa0462c3acffdacf2f4e819e05ad39b13f2de5a36ac8f7b8467c639a0db

--- src/random.cpp
+++ src/random.cpp
@@ -12,14 +12,15 @@
 #include <compat.h> // for Windows API
 #include <wincrypt.h>
 #endif
-#include <util.h>             // for LogPrint()
-#include <utilstrencodings.h> // for GetTime()
+#include <logging.h>  // for LogPrint()
+#include <utiltime.h> // for GetTime()
 
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
 
 #ifndef WIN32
+#include <fcntl.h>
 #include <sys/time.h>
 #endif
 
