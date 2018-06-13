f8ea6901002235043c55f3a8d8e74371166f6039
MarcoFalke <falke.marco@gmail.com>
2018-06-13 13:21:00 -0400

    Merge #13406: travis: Change Mac goal to all deploy
    3d69853090 travis: Change Mac goal to all deploy so that travis can build all executables for Mac. (Chun Kuan Lee)
    
    Pull request description:
    
      Currently, travis only build bitcoin-qt for Mac with make deploy, so  I think that we can add a make step to build cli, daemon, test and bench executables.
    
    Tree-SHA512: 346c207836ee7313a57e0cd03367ada7e95c182d0f58b06c4ee5b8f2865ba4ee077b030a5494b58a808c015afe0569fa3252d0a5a687c47b7094dc77599ce50e
    Signed-off-by: pasta <pasta@dashboost.org>
    
    # Conflicts:
    #	.travis.yml

--- ci/matrix.sh
+++ ci/matrix.sh
@@ -82,5 +82,5 @@ elif [ "$BUILD_TARGET" = "mac" ]; then
   export HOST=x86_64-apple-darwin16
   export BITCOIN_CONFIG="--enable-gui --enable-reduce-exports --disable-miner"
   export OSX_SDK=10.11
-  export GOAL="deploy"
+  export GOAL="all deploy"
 fi
