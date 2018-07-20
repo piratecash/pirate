00168d17545ff57a012f8a0eed96931ac9c9b27e
Wladimir J. van der Laan <laanwj@gmail.com>
2018-07-20 16:27:31 +0200

    Merge #13482: Remove boost::program_options dependency
    f447a0a7079619f0d650084df192781cca9fd826 Remove program options from build system (Chun Kuan Lee)
    11588c639e8912f1b28e981c1a2a0e4306dbd093 Replace boost program_options (Chun Kuan Lee)
    
    Pull request description:
    
      Concept from #12744, but without parsing negated options.
    
    Tree-SHA512: 7f418744bb8934e313d77a5f162633746ef5d043de802b9c9cd9f7c1842e7e566eb5f171cd9e2cc13317281b2449c6fbd553fa4f09b837e6af2f5d2b2aabdca2

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -77,8 +77,6 @@ EXPECTED_BOOST_INCLUDES=(
     boost/pool/pool_alloc.hpp
     boost/preprocessor/cat.hpp
     boost/preprocessor/stringize.hpp
-    boost/program_options/detail/config_file.hpp
-    boost/program_options/parsers.hpp
     boost/scoped_array.hpp
     boost/signals2/last_value.hpp
     boost/signals2/signal.hpp
