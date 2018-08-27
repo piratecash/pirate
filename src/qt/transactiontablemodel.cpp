9ba8b9a90a1ec16e5f5958f682f48d59554d2b02
MarcoFalke <falke.marco@gmail.com>
2018-08-27 13:32:59 -0400

    Merge #13769: Mark single-argument constructors "explicit"
    1ac3c983bf Mark single-argument constructors "explicit" (practicalswift)
    
    Pull request description:
    
      Mark single-argument constructors `explicit`.
    
      Rationale:
      * Avoid unexpected implicit promotions.
    
      From the developer notes:
    
      > **By default, declare single-argument constructors explicit.**
      > Rationale: This is a precaution to avoid unintended conversions that might arise when single-argument constructors are used as implicit conversion functions.
    
    Tree-SHA512: 7901ed5be808c9d0ecb5ca501e1bc0395987fe1b7941b8548cebac2ff08a14f7dab61fab374a69b9ba29a9295a04245c814325c7f95b97ae558af0780f111dfa

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -57,7 +57,7 @@ struct TxLessThan
 class TransactionTablePriv
 {
 public:
-    TransactionTablePriv(TransactionTableModel *_parent) :
+    explicit TransactionTablePriv(TransactionTableModel *_parent) :
         parent(_parent)
     {
     }
