6b5e4c5c121e28da00bac805f487a028f12fc38a
Wladimir J. van der Laan <laanwj@gmail.com>
2018-09-04 12:21:16 +0200

    Merge #14140: [Net] Switch nPrevNodeCount to vNodesSize
    83d53058ae240a5c396ac927972ad96d2a11e903 Switch nPrevNodeCount to vNodesSize. (Patrick Strateman)
    
    Pull request description:
    
      These both have the same value, but the variable naming is confusing.
    
    Tree-SHA512: 4f645e89efdc69884ff4c8bbcf42e2b35d2733687c0fc6ab3f0797e0141fe23ef9cde8bb6ba422f47a88f554e55a099b1f0b3f47cb9fde12db3d46b9a0041bb0

--- src/net.cpp
+++ src/net.cpp
@@ -1379,7 +1379,7 @@ void CConnman::NotifyNumConnectionsChanged()
     if(vNodesSize != nPrevNodeCount) {
         nPrevNodeCount = vNodesSize;
         if(clientInterface)
-            clientInterface->NotifyNumConnectionsChanged(nPrevNodeCount);
+            clientInterface->NotifyNumConnectionsChanged(vNodesSize);
     }
 }
 
