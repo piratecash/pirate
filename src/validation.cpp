0a38c76cc0d2540ebfce93677bed5c523d4588cb
MarcoFalke <falke.marco@gmail.com>
2018-11-27 12:43:31 -0500

    Merge #14772: refactor: Convert comments to thread safety annotations
    fa71eb5196 Convert comments to thread safety annotations (MarcoFalke)
    
    Pull request description:
    
      Convert human-only-readable annotations to human-and-machine-readable annotations.
    
    Tree-SHA512: 98a97b32b50e8ec816626ed853d56c9afb31065eb14f058cda8f22a16ddd2b0c02911f9e30478aa347f466891f972496d89a828eb6e4662fb712d7d9e7643d49

--- src/validation.cpp
+++ src/validation.cpp
@@ -1932,8 +1932,7 @@ void StopScriptCheckWorkerThreads()
     scriptcheckqueue.StopWorkerThreads();
 }
 
-// Protected by cs_main
-VersionBitsCache versionbitscache;
+VersionBitsCache versionbitscache GUARDED_BY(cs_main);
 
 int32_t ComputeBlockVersion(const CBlockIndex* pindexPrev, const Consensus::Params& params, bool fCheckMasternodesUpgraded, bool isPos)
 {
@@ -2005,8 +2004,7 @@ public:
     }
 };
 
-// Protected by cs_main
-static ThresholdConditionCache warningcache[VERSIONBITS_NUM_BITS];
+static ThresholdConditionCache warningcache[VERSIONBITS_NUM_BITS] GUARDED_BY(cs_main);
 
 static unsigned int GetBlockScriptFlags(const CBlockIndex* pindex, const Consensus::Params& consensusparams) EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
     AssertLockHeld(cs_main);
