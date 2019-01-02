23d508fd16ea3c90ea6af97604f7d08355e15d22
Wladimir J. van der Laan <laanwj@gmail.com>
2019-01-02 14:42:21 +0100

    Merge #14457: test: add invalid tx templates for use in functional tests
    59e387705c7e55ec40400301346354fa2d0c613f test: add invalid tx templates for use in functional tests (James O'Beirne)
    
    Pull request description:
    
      This change adds a list of `CTransaction`-generating templates which each correspond to a specific type of invalid transaction. We then use this list to test for a wider variety of invalid tx types in `p2p_invalid_tx.py` and `feature_block.py`.
    
      Consolidating all invalid tx types will allow us to more easily cover all tx reject cases from a variety of tests without repeating ourselves. Validation logic doesn't differ much between mempool and block acceptance, but there *is* a difference and we should be sure we're testing both comprehensively.
    
      Right now, I've only added templates covering the tx reject types listed below but if this approach seems worthwhile I will expand the list to be fully comprehensive.
      ```
      bad-txns-in-belowout
      bad-txns-inputs-duplicate
      bad-txns-too-many-sigops
      bad-txns-vin-empty
      bad-txns-vout-empty
      bad-txns-vout-negative
      ```
    
    Tree-SHA512: 05407f4a953fbd7c44c08bb49bb989cefd39a2b05ea00f5b3c92197a3f05e1b302f789e33832445734220e1c333d133aba385740b77b84139b170c583471ce20

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -18,6 +18,8 @@ from .script import CScript, OP_TRUE, OP_CHECKSIG
 from .util import assert_equal, hex_str_to_bytes
 from io import BytesIO
 
+MAX_BLOCK_SIGOPS = 20000
+
 def create_block(hashprev, coinbase, ntime=None):
     """Create a block (with regtest difficulty)."""
     block = CBlock()
