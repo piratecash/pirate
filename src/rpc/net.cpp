34db4b24cc6d3f27a91c56e3ece3208c3af9f8ee
Wladimir J. van der Laan <laanwj@gmail.com>
2019-01-07 15:41:14 +0100

    Merge #15078: rpc: Document bytessent_per_msg and bytesrecv_per_msg
    fab3f1467823c854cc88bc5a7e087263866ed87e rpc: Document bytessent_per_msg and bytesrecv_per_msg (MarcoFalke)
    
    Pull request description:
    
    Tree-SHA512: 11af7502933b3dae203d90e36b35019e0ebe67ee09aa77360a27547487bd2b6bcaa18c5f60bc21966291d2ccf44dfedebf40103c4db70a359400f535a66abb23

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2014-2019 The Dash Core developers
+// Copyright (c) 2009-2019 The Bitcoin Core developers
+// Copyright (c) 2014-2021 The Dash Core developers
 // Copyright (c) 2020-2022 The Cosanta Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -115,11 +115,15 @@ static UniValue getpeerinfo(const JSONRPCRequest& request)
             "    ],\n"
             "    \"whitelisted\": true|false, (boolean) Whether the peer is whitelisted\n"
             "    \"bytessent_per_msg\": {\n"
-            "       \"addr\": n,              (numeric) The total bytes sent aggregated by message type\n"
+            "       \"msg\": n,               (numeric) The total bytes sent aggregated by message type\n"
+            "                               When a message type is not listed in this json object, the bytes sent are 0.\n"
+            "                               Only known message types can appear as keys in the object.\n"
             "       ...\n"
             "    },\n"
             "    \"bytesrecv_per_msg\": {\n"
-            "       \"addr\": n,              (numeric) The total bytes received aggregated by message type\n"
+            "       \"msg\": n,               (numeric) The total bytes received aggregated by message type\n"
+            "                               When a message type is not listed in this json object, the bytes received are 0.\n"
+            "                               Only known message types can appear as keys in the object and all bytes received of unknown message types are listed under '"+NET_MESSAGE_COMMAND_OTHER+"'.\n"
             "       ...\n"
             "    }\n"
             "  }\n"
@@ -198,14 +202,14 @@ static UniValue getpeerinfo(const JSONRPCRequest& request)
         obj.pushKV("permissions", permissions);
 
         UniValue sendPerMsgCmd(UniValue::VOBJ);
-        for (const mapMsgCmdSize::value_type &i : stats.mapSendBytesPerMsgCmd) {
+        for (const auto& i : stats.mapSendBytesPerMsgCmd) {
             if (i.second > 0)
                 sendPerMsgCmd.pushKV(i.first, i.second);
         }
         obj.pushKV("bytessent_per_msg", sendPerMsgCmd);
 
         UniValue recvPerMsgCmd(UniValue::VOBJ);
-        for (const mapMsgCmdSize::value_type &i : stats.mapRecvBytesPerMsgCmd) {
+        for (const auto& i : stats.mapRecvBytesPerMsgCmd) {
             if (i.second > 0)
                 recvPerMsgCmd.pushKV(i.first, i.second);
         }
