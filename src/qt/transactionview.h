91ebf3ac043ed6faa9684292e781e9fd70f4ba79
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2019-03-17 19:55:56 +0100

    merge bitcoin#16706: Replace deprecated QSignalMapper by lambda expressions

--- src/qt/transactionview.h
+++ src/qt/transactionview.h
@@ -24,7 +24,6 @@ class QItemSelectionModel;
 class QLineEdit;
 class QMenu;
 class QModelIndex;
-class QSignalMapper;
 class QTableView;
 QT_END_NAMESPACE
 
@@ -72,7 +71,6 @@ private:
     QLineEdit *amountWidget;
 
     QMenu *contextMenu;
-    QSignalMapper *mapperThirdPartyTxUrls;
 
     QFrame *dateRangeWidget;
     QDateTimeEdit *dateFrom;
