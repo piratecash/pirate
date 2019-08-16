dc9d6330e12b80dfb417746210b03125adbc0183
MarcoFalke <falke.marco@gmail.com>
2019-08-16 10:17:20 -0400

    Merge #16618: [Fix] Allow connection of a noban banned peer
    d117f4541d4717e83c9396273e92960723622030 Add test for setban (nicolas.dorier)
    dc7529abf0197dccb876dc4a93cbdd2ad9f03e5c [Fix] Allow connection of a noban banned peer (nicolas.dorier)
    
    Pull request description:
    
      Reported by @MarcoFalke on https://github.com/bitcoin/bitcoin/pull/16248#discussion_r314026195
    
      The bug would mean that if the peer connecting to you is banned, but whitelisted without specific permissions, it would not be able to connect to the node.
    
      The solution is just to move the same line below.
    
    ACKs for top commit:
      Sjors:
        Agree inline is more clear. utACK d117f45
      MarcoFalke:
        ACK d117f4541d4717e83c9396273e92960723622030
    
    Tree-SHA512: 0fed39acb1e8db67bb0bf4c4de3ad034ae776f38d55bd661f1ae0e1a4c6becaf1824ab46ed8279f2f31df3f4b29ff56461d8b167d3e9cece62cfe58b5a912811

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -135,6 +135,7 @@ BASE_SCRIPTS = [
     'wallet_keypool.py',
     'wallet_keypool_hd.py',
     'p2p_mempool.py',
+    'rpc_setban.py',
     'mining_prioritisetransaction.py',
     'p2p_invalid_block.py',
     'p2p_invalid_tx.py',
