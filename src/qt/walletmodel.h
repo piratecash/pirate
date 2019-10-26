bf11b940eed76fea0a3ba3068cf8ef1511d9424a
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-10-26 13:00:01 +0200

    Merge #17120: gui: Fix start timer from non QThread
    a8f5026d6d992fd8d72908c848c5028f0f9a8cd1 gui: Fix start timer from non QThread (João Barbosa)
    
    Pull request description:
    
      Fixes #16296.
    
    ACKs for top commit:
      laanwj:
        code review ACK a8f5026d6d992fd8d72908c848c5028f0f9a8cd1
    
    Tree-SHA512: d7b05ac88e188de16cbbe80cb2f773b7976ee07ee876ac94a93f9351856c4f3a9d66a531d3f3748d2dccff8c8d77d9d8227433069ed5909c32be2efeaa32f655

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -261,8 +261,6 @@ private:
     int cachedNumISLocks;
     int cachedCoinJoinRounds;
 
-    QTimer *pollTimer;
-
     void subscribeToCoreSignals();
     void unsubscribeFromCoreSignals();
     void checkBalanceChanged(const interfaces::WalletBalances& new_balances);
@@ -298,6 +296,9 @@ Q_SIGNALS:
     void canGetAddressesChanged();
 
 public Q_SLOTS:
+    /* Starts a timer to periodically update the balance */
+    void startPollBalance();
+
     /* Wallet status might have changed */
     void updateStatus();
     /* New transaction, or transaction changed status */
