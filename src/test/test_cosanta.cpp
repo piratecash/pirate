461489cd9bcb7efffd8acce3dd33161c13f8b398
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-01-31 04:57:21 -0600

    Add unit tests for CPrivateSend::IsCollateralAmount (#3310)
    * add privatesend collateral tests
    
    Signed-off-by: Pasta <pasta@dashboost.org>
    
    * move privatesend_tests.cpp in Makefile.test.include
    
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/test/test_cosanta.cpp
+++ src/test/test_cosanta.cpp
@@ -27,6 +27,7 @@
 #include "evo/deterministicmns.h"
 #include "evo/cbtx.h"
 #include "llmq/quorums_init.h"
+#include "privatesend/privatesend.h"
 
 #include <memory>
 
@@ -58,6 +59,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
         SetupNetworking();
         InitSignatureCache();
         InitScriptExecutionCache();
+        CPrivateSend::InitStandardDenominations();
         fPrintToDebugLog = false; // don't want to write to debug.log file
         fCheckBlockIndex = true;
         SelectParams(chainName);
