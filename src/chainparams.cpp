55347a93b07e47b30d31e42b2da0a7f8fefe95bc
Alexander Block <ablock84@gmail.com>
2020-03-17 07:08:37 +0100

    Implement new way of concentrated signature recovery
    Instead of propagating all sig shares to all LLMQ members, this will now
    make all members send their individual sig share to a single member, which
    is then responsible for the recovery and propagation of the recovered
    signature. This process is repeated by all members every second for another
    target/recovering member, until a recovered signature appears.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -226,6 +226,7 @@ static Consensus::LLMQParams llmq_test = {
         .signingActiveQuorumCount = 2, // just a few ones to allow easier testing
 
         .keepOldConnections = 3,
+        .recoveryMembers = 3,
 };
 
 // this one is for devnets only
@@ -245,6 +246,7 @@ static Consensus::LLMQParams llmq_devnet = {
         .signingActiveQuorumCount = 3, // just a few ones to allow easier testing
 
         .keepOldConnections = 4,
+        .recoveryMembers = 6,
 };
 
 static Consensus::LLMQParams llmq50_60 = {
@@ -263,6 +265,7 @@ static Consensus::LLMQParams llmq50_60 = {
         .signingActiveQuorumCount = 24, // a full day worth of LLMQs
 
         .keepOldConnections = 25,
+        .recoveryMembers = 25,
 };
 
 static Consensus::LLMQParams llmq400_60 = {
@@ -281,6 +284,7 @@ static Consensus::LLMQParams llmq400_60 = {
         .signingActiveQuorumCount = 4, // two days worth of LLMQs
 
         .keepOldConnections = 5,
+        .recoveryMembers = 100,
 };
 
 // Used for deployment and min-proto-version signalling, so it needs a higher threshold
@@ -300,6 +304,7 @@ static Consensus::LLMQParams llmq400_85 = {
         .signingActiveQuorumCount = 4, // four days worth of LLMQs
 
         .keepOldConnections = 5,
+        .recoveryMembers = 100,
 };
 
 
--- src/protocol.cpp
+++ src/protocol.cpp
@@ -72,6 +72,7 @@ const char *QSIGSHARESINV="qsigsinv";
 const char *QGETSIGSHARES="qgetsigs";
 const char *QBSIGSHARES="qbsigs";
 const char *QSIGREC="qsigrec";
+const char *QSIGSHARE="qsigshare";
 const char *CLSIG="clsig";
 const char *ISLOCK="islock";
 const char *MNAUTH="mnauth";
@@ -138,6 +139,7 @@ const static std::string allNetMessageTypes[] = {
     NetMsgType::QGETSIGSHARES,
     NetMsgType::QBSIGSHARES,
     NetMsgType::QSIGREC,
+    NetMsgType::QSIGSHARE,
     NetMsgType::CLSIG,
     NetMsgType::ISLOCK,
     NetMsgType::MNAUTH,
