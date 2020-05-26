2588257dfb3bd2bfa87f6721b8afa698eadd0121
fanquake <fanquake@gmail.com>
2020-05-26 08:30:35 +0800

    Merge #19059: gui: update Qt base translations for macOS release
    69bfcac27a83440092bc6e61904ded910ed4baf4 gui: update Qt base translations for macOS release (fanquake)
    
    Pull request description:
    
      These haven't been updated since their addition, so this updates the list that
      controls which qt base translations are bundled with the macOS binary, to all the
      languages that are available with qt 5.9.8.
    
      This could probably be improved in some way, however qt updates are infrequent,
      and I didn't want to spend any more time looking at this. Also given that no-one
      seems to have noticed and/or reported this it wouldn't seem high-priority.
    
      Could be backported to 0.20.1.
    
      Master:
      ![master](https://user-images.githubusercontent.com/863730/82729428-11bce200-9d2a-11ea-8569-ee65d46c7403.png)
    
      This PR:
      ![fixed](https://user-images.githubusercontent.com/863730/82729427-0f5a8800-9d2a-11ea-86dd-1e6a3e211efa.png)
    
    ACKs for top commit:
      hebasto:
        ACK 69bfcac27a83440092bc6e61904ded910ed4baf4, tested on macOS 10.15.
    
    Tree-SHA512: df142fb16097deb514e72e005b73aafc4eb4ff0c17e423ba5040a3ec6874020a733e1c5259a88923580e71ef73c16222aed28f482b8c270a544a85b745a7b327

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -10,6 +10,7 @@ $(package)_build_subdir=qtbase
 $(package)_qt_libs=corelib network widgets gui plugins testlib
 $(package)_patches=fix_qt_pkgconfig.patch mac-qmake.conf fix_configure_mac.patch fix_no_printer.patch fix_rcc_determinism.patch xkb-default.patch no-xlib.patch fix_limits_header.patch
 
+# Update OSX_QT_TRANSLATIONS when this is updated
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
 $(package)_qttranslations_sha256_hash=b36da7d93c3ab6fca56b32053bb73bc619c8b192bb89b74e3bcde2705f1c2a14
 
