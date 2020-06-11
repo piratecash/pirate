008ff469c6898f6963bdf31147c83444ff07be1d
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-06-11 11:39:23 +0300

    Fix chainlock scheduler handling (#3514)
    Reindexing corrupted chain always fails otherwise.

--- src/llmq/quorums_chainlocks.cpp
+++ src/llmq/quorums_chainlocks.cpp
@@ -41,6 +41,8 @@ CChainLocksHandler::CChainLocksHandler()
 
 CChainLocksHandler::~CChainLocksHandler()
 {
+    scheduler_thread->interrupt();
+    scheduler_thread->join();
     delete scheduler_thread;
     delete scheduler;
 }
@@ -58,9 +60,8 @@ void CChainLocksHandler::Start()
 
 void CChainLocksHandler::Stop()
 {
+    scheduler->stop();
     quorumSigningManager->UnregisterRecoveredSigsListener(this);
-    scheduler_thread->interrupt();
-    scheduler_thread->join();
 }
 
 bool CChainLocksHandler::AlreadyHave(const CInv& inv)
