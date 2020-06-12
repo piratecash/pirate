d1e40005c247968b0e35151e47f9e94cb09ecc8a
Pasta <pasta@dashboost.org>
2020-06-12 10:48:02 -0500

    initialize fAnonymizableTallyCached and fAnonymizableTallyCachedNonDenom
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -753,9 +753,9 @@ private:
     int64_t nLastResend = 0;
     bool fBroadcastTransactions = false;
 
-    mutable bool fAnonymizableTallyCached;
+    mutable bool fAnonymizableTallyCached = false;
     mutable std::vector<CompactTallyItem> vecAnonymizableTallyCached;
-    mutable bool fAnonymizableTallyCachedNonDenom;
+    mutable bool fAnonymizableTallyCachedNonDenom = false;
     mutable std::vector<CompactTallyItem> vecAnonymizableTallyCachedNonDenom;
 
     /**
