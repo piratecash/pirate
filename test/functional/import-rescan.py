d77eb1429b50bd192b3c840c5e840534f784e7c3
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-07-08 23:28:30 +0000

    More pruning improvements (#3579)
    * explicitly check that -disablegovernance is true for pruned mode.
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * try to set -disablegovernance and -txindex if pruned mode is enabled
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * Adjust InitWarning when governance validation is disabled
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * Check for deprecated -litemode earlier
    
    * Move -disablegovernance warning into AppInitParameterInteraction
    
    * Tweak -prune help text and a related comment in AppInitParameterInteraction
    
    * Ignore init warnings in blockchain.py
    
    * Adjust "governance validation disabled" warning depending on if running a pruned node
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * Skip init warnings in import-rescan.py and node_network_limited.py
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- test/functional/import-rescan.py
+++ test/functional/import-rescan.py
@@ -26,6 +26,7 @@ from test_framework.util import (assert_raises_rpc_error, connect_nodes, assert_
 import collections
 import enum
 import itertools
+import sys
 
 Call = enum.Enum("Call", "single multi")
 Data = enum.Enum("Data", "address pub priv")
@@ -126,7 +127,7 @@ class ImportRescanTest(BitcoinTestFramework):
                 # txindex is enabled by default in Cosanta and needs to be disabled for import-rescan.py
                 extra_args[i] += ["-prune=1", "-txindex=0", "-reindex"]
 
-        self.add_nodes(self.num_nodes, extra_args)
+        self.add_nodes(self.num_nodes, extra_args, stderr=sys.stdout)
         self.start_nodes()
         for i in range(1, self.num_nodes):
             connect_nodes(self.nodes[i], 0)
