1beffa9aebe00cde74842a424c9dc7a61d891a29
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-09-17 14:42:47 +0300

    qt: Make sure send confirmation dialog uses correct font settings (#3714)

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -976,6 +976,7 @@ SendConfirmationDialog::SendConfirmationDialog(const QString &title, const QStri
     QWidget *parent) :
     QMessageBox(QMessageBox::Question, title, text, QMessageBox::Yes | QMessageBox::Cancel, parent), secDelay(_secDelay)
 {
+    GUIUtil::updateFonts();
     setDefaultButton(QMessageBox::Cancel);
     yesButton = button(QMessageBox::Yes);
     updateYesButton();
