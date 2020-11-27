3e5b5424fe1f6586dd447d1ab7cc46506072ad92
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-11-27 17:59:07 +0300

    Fix a couple of issues with `-rescan` (#3828)
    * Update key metadata when we find a key which appears to be used earlier than we expected
    
    * Introduce second mode for `-rescan` to be able to rescan from genesis block
    
    * Add `-rescan=2` option to Repair tab
    
    * Expose `nTimeFirstKey` in `CreateWalletFromFile` logs and in `getwalletinfo` rpc
    
    * fix: check `pIndex != nullptr`
    
    * Ignore incorrect -rescan values

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -101,7 +101,8 @@ public Q_SLOTS:
 
     /** Wallet repair options */
     void walletSalvage();
-    void walletRescan();
+    void walletRescan1();
+    void walletRescan2();
     void walletZaptxes1();
     void walletZaptxes2();
     void walletUpgrade();
