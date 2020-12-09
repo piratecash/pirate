4d5ae756ce6fe31e8b0b7241e40d9b9eba2c8779
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-12-10 02:08:05 +0300

    consenus: Implement LLMQ_100_67 quorums (#3844)
    * Add LLMQ_100_67 quorums
    
    * Re-use DEPLOYMENT_V17 bit to activate LLMQ_100_67 quorums
    
    * Add LLMQ_TEST_NEW quorum and test its activation
    
    * Tweak mine_quorum to work correctly with multiple quorum types
    
    And to avoid a potentialy endless "while" loop
    
    * llmq: Rename IsQuorumTypeEnabledAtBlock -> IsQuorumTypeEnabled
    
    * chainparams|test: Rename llmq_test_new -> llmq_test_v17
    
    * chainparams|consensus|llmq: Rename LLMQ_TEST_NEW -> LLMQ_TEST_V17
    
    * Tweak few strings and the name of the test
    
    * llmq: Make GetEnabledQuorumTypes return a vector of LLMQTypes, introduce GetLLMQParams
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * Tweak minSize
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    * Exclude LLMQ_100_67 from Concentrated Recovery
    
    * Update test/functional/feature_new_quorum_type_activation.py
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    Co-authored-by: xdustinface <xdustinfacex@gmail.com>
    Co-authored-by: pasta <pasta@dashboost.org>
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

--- src/miner.cpp
+++ src/miner.cpp
@@ -204,9 +204,9 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(
                         ? nMedianTimePast
                         : pblock->GetBlockTime();
         if (fDIP0003Active_context) {
-            for (auto& p : chainparams.GetConsensus().llmqs) {
+            for (const Consensus::LLMQType& type : llmq::CLLMQUtils::GetEnabledQuorumTypes(pindexPrev)) {
                 CTransactionRef qcTx;
-                if (llmq::quorumBlockProcessor->GetMinableCommitmentTx(p.first, nHeight, qcTx)) {
+                if (llmq::quorumBlockProcessor->GetMinableCommitmentTx(type, nHeight, qcTx)) {
                     pblock->vtx.emplace_back(qcTx);
                     pblocktemplate->vTxFees.emplace_back(0);
                     pblocktemplate->vTxSigOps.emplace_back(0);
