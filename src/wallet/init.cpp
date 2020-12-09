f9603d077dd50d9e89a16d061bbe1a00d6177dc6
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-12-09 21:29:31 +0100

    wallet|init: Fix forcing -disablewallet if -masternodeblsprivkey is set (#3858)
    Prior to this `-disablewallet` is getting forced after the UI has been initialised if a `-masternodeblsprivkey` is set. This leads to the application running with a "Wallet UI" with undefined behaviour and not with the the expected "Node only UI".

--- src/wallet/init.cpp
+++ src/wallet/init.cpp
@@ -119,10 +119,6 @@ void WalletInit::AddWalletOptions() const
 
 bool WalletInit::ParameterInteraction() const
 {
-    if (gArgs.IsArgSet("-masternodeblsprivkey") && gArgs.SoftSetBoolArg("-disablewallet", true)) {
-        LogPrintf("%s: parameter interaction: -masternodeblsprivkey set -> setting -disablewallet=1\n", __func__);
-    }
-
     if (gArgs.GetBoolArg("-disablewallet", DEFAULT_DISABLE_WALLET)) {
         for (const std::string& wallet : gArgs.GetArgs("-wallet")) {
             LogPrintf("%s: parameter interaction: -disablewallet -> ignoring -wallet=%s\n", __func__, wallet);
