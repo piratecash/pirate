c6331a2f2742503c231643ae51bba6c5b6975899
tomthoros <tom.laigna@gmail.com>
2020-12-12 00:17:10 +0200

    Consensus: DIP-0020: Dash opcode updates - OP_CAT and OP_SPLIT. (#3824)
    * DIP-0020: Dash opcode updates - enable OP_CAT and OP_SPLIT (renamed from OP_SUBSTR)
    
    * DIP-0020: Dash opcode updates - DEPLOYMENT_V17 activates dip0020 opcodes
    
    * Add/tweak MAX_SCRIPT_ELEMENT_SIZE tests for OP_CAT and OP_SPLIT
    
    * Check nDefaultMaxNumSize in OP_SPLIT tests
    
    * Purify DISABLED_OPCODE tests for OP_CAT and OP_SPLIT, fix DoTest to actually preserve the DIP0020 flag
    
    * Fix `warning: '&' within '|' [-Wbitwise-op-parentheses]`
    
    * Rework/simplify feature_dip0020_activation.py
    
    * DIP-0020: Remove functionally redundant tests.
    
    * Fix file permissions for feature_dip0020_activation.py
    
    * DIP-0020: fix typo
    
    * Update src/test/script_tests.cpp
    
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    
    * Update test/functional/feature_dip0020_activation.py
    
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    
    * DIP-0020: improve comments
    
    * DIP-0020: dont use negative booleans
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -165,6 +165,7 @@ BASE_SCRIPTS= [
     'rpc_verifyislock.py',
     'p2p_fingerprint.py',
     'rpc_platform_filter.py',
+    'feature_dip0020_activation.py',
     'feature_uacomment.py',
     'p2p_unrequested_blocks.py',
     'feature_logging.py',
