662fee263cf503d7b18ff3aad48033585e9c728a
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-12-12 01:38:08 +0300

    Refactor GetRequiredPaymentsString / `masternode winners` (#3861)
    * rpc: Refactor GetRequiredPaymentsString / `masternode winners`
    
    Move (and refactor) GetRequiredPaymentsString(s) from masternode-payments.{h,cpp} and governance-classes.{cpp,h} into rpc/masternode.cpp. These functions aren't used anywhere else.
    
    * Update src/rpc/masternode.cpp
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

--- src/governance/governance-classes.h
+++ src/governance/governance-classes.h
@@ -64,7 +64,6 @@ public:
     static bool GetSuperblockPayments(int nBlockHeight, std::vector<CTxOut>& voutSuperblockRet);
     static void ExecuteBestSuperblock(int nBlockHeight);
 
-    static std::string GetRequiredPaymentsString(int nBlockHeight);
     static bool IsValid(const CTransaction& txNew, int nBlockHeight, CAmount blockReward);
 };
 
