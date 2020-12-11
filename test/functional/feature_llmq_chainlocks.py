78b67181411be45324a67331acb087de062bd968
pasta <pasta@dashboost.org>
2020-12-10 20:25:55 -0600

    Fix #10579
    Signed-off-by: pasta <pasta@dashboost.org>

--- test/functional/feature_llmq_chainlocks.py
+++ test/functional/feature_llmq_chainlocks.py
@@ -149,7 +149,7 @@ class LLMQChainLocksTest(CosantaTestFramework):
         }
 
         rawtx = node.createrawtransaction(inputs, outputs)
-        rawtx = node.signrawtransaction(rawtx)
+        rawtx = node.signrawtransactionwithwallet(rawtx)
         rawtxid = node.sendrawtransaction(rawtx["hex"])
 
         return [txid, rawtxid]
