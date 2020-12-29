51717776f6ce887427fcbff96926a9dbf3844c7b
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-12-29 13:30:50 +0300

    Fix versionbits cache usage in IsQuorumTypeEnabled (#3901)

--- src/llmq/quorums_init.cpp
+++ src/llmq/quorums_init.cpp
@@ -14,6 +14,7 @@
 #include <llmq/quorums_instantsend.h>
 #include <llmq/quorums_signing.h>
 #include <llmq/quorums_signing_shares.h>
+#include <llmq/quorums_utils.h>
 
 #include <dbwrapper.h>
 
@@ -61,6 +62,8 @@ void DestroyLLMQSystem()
     blsWorker = nullptr;
     delete llmqDb;
     llmqDb = nullptr;
+    LOCK(cs_llmq_vbc);
+    llmq_versionbitscache.Clear();
 }
 
 void StartLLMQSystem()
