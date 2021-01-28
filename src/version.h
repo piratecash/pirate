66a0aaac90012f1298c64b70028dad458bd44518
dustinface <35775977+xdustinface@users.noreply.github.com>
2021-01-28 23:33:18 +0100

    llmq|rpc|test|version: Implement P2P messages QGETDATA <-> QDATA  (#3953)
    * version: Bump PROTOCOL_VERSION and MIN_MASTERNODE_PROTO_VERSION
    
    * version: Introduce LLMQ_DATA_MESSAGES_VERSION for QGETDATA/QDATA support
    
    * test: Bump MY_VERSION to 70219 (LLMQ_DATA_MESSAGES_VERSION)
    
    * llmq: Introduce CQuorumDataRequest as wrapper for QGETDATA requests
    
    * llmq: Implement CQuorum::{SetVerificationVector, SetSecretKeyShare}
    
    * llmq|net|protocol: Implement QGETDATA/QDATA P2P messages
    
    * llmq: Restrict processing QGETDATA/QDATA to masternodes only
    
    * llmq: Implement request limiting for QGETDATA/QDATA
    
    * llmq: Implement CQuorumManger::RequestQuorumData
    
    * rpc: Implement "quorum getdata" as wrapper around QGETDATA
    
    Allows to trigger sending QGETDATA messages to connected peers by RPC.
    
    * test: Handle QGETDATA/QDATA messages in mininode
    
    * test: Add data structures to support QGETDATA/QDATA
    
    * test: Add some helper in test_framework.py
    
    * test: Implement tests for QGETDATA/QDATA in p2p_quorum_data.py
    
    * test: Add p2p_quorum_data.py to BASE_SCRIPTS
    
    * llmq|test: Add QWATCH support for QGETDATA/QDATA
    
    * llmq: Store CQuorumPtr in cache, not CQuorumCPtr
    
    * llmq: Fix cache usage after recent changes
    
    * Use uacomment to create/find specific p2ps
    
    * No need to use network adjusted time here, GetTime should be enough
    
    * rpc: check proTxHash
    
    * minor tweaks
    
    * test: Adjustments after 4e27d6513e0073ed848ede262cfec82a9134abc0
    
    * llmq: Rename and improve error lambda in CQuorumManager::ProcessMessage
    
    * llmq: Process QDATA if -watchquorums is enabled
    
    * test: Handle qwatch messages in mininode
    
    * test: Add test for -watchquorums support
    
    * test: Just some empty lines
    
    * test: Properly stop the p2p network thread at the end of the test
    
    * rpc: Adjust "quorum getdata" parameter descriptions
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    * rpc: Fix optionality of proTxHash in "quorum getdata" command
    
    * test: Test optionality of proTxHash for "quorum getdata" command
    
    * test: Be more specific about imports in p2p_quorum_data.py
    
    * llmq|rpc: Add some comments about the request.GetDataMask checks
    
    * test: Some more empty lines
    
    * rpc: One more parameter description
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    * test: Unify assert statements / drop parentheses for all of them
    
    * fix typo
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * adjust some line wrapping to 80 chars
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * tests: Seperate out into dif atomic methods, add logging
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * test: Avoid restarting masternodes, just let available requests expire
    
    Just takes a lot time and isn't required imo.
    
    * test: Drop redundant code/tests after separation
    
    This was introduced in 9e224ec2f2ef4a58adaf0f9d4ffe110e379718ef
    
    * test: Merge three tests
    
    "test_mnauth_restriction", "test_invalid_messages" and "test_invalid_unexpected_qdata" with the resulting name "test_basics" because i don't feel like DKG recovery thing should be part of a test called "test_invalid_messages" and giving it an own test probably wouldn't make a lot sense because it would still depend on "test_invalid_messages". I also think there is no need for a separated "test_invalid_unexpected_qdata".
    
    * test: Rename test_ratelimiting_banscore -> test_request_limit
    
    * test: Apply python style
    
    * test: Wrap all at 120 characters
    
    Thats the default "draw annoying warnings" setting for PyCharm (and IMO a reasonable line length).
    
    * test: Move some variables
    
    * test: Optimize for speed
    
    * tests: use wait_until in get_mininode_id
    
    * test: Don't use `!=` to check for `None`
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    Co-authored-by: pasta <pasta@dashboost.org>

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70222;
+static const int PROTOCOL_VERSION = 70219;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,9 +22,6 @@ static const int MIN_PEER_PROTO_VERSION = 70222;
 
 //! minimum proto version of masternode to accept in DKGs
 static const int MIN_MASTERNODE_PROTO_VERSION = 70222;
-
-//! minimum proto version of masternode to accept in DKGs
-static const int MIN_MASTERNODE_PROTO_VERSION = 70217;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -39,5 +36,8 @@ static const int SENDDSQUEUE_PROTO_VERSION = 70214;
 
 //! protocol version is included in MNAUTH starting with this version
 static const int MNAUTH_NODE_VER_VERSION = 70218;
+
+//! introduction of QGETDATA/QDATA messages
+static const int LLMQ_DATA_MESSAGES_VERSION = 70219;
 
 #endif // BITCOIN_VERSION_H
