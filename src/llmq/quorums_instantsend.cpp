53d79bcd3c804f2ce6946ad1431fb821940aadea
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2021-03-31 16:01:59 -0400

    trivial: Remove unneeded include from quorums_instantsend.cpp (#4073)
    * Remove unneeded include from quorums_instantsend.cpp
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * circular depends -6

--- src/llmq/quorums_instantsend.cpp
+++ src/llmq/quorums_instantsend.cpp
@@ -15,10 +15,6 @@
 #include <spork.h>
 #include <validation.h>
 
-#ifdef ENABLE_WALLET
-#include <wallet/wallet.h>
-#endif
-
 #include <cxxtimer.hpp>
 
 #include <boost/algorithm/string/replace.hpp>
