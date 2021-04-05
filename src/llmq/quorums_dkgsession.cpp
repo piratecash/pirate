a40566250b241ccf63d479c15caee310ead95614
dustinface <35775977+xdustinface@users.noreply.github.com>
2021-04-05 21:26:22 +0200

    llmq: Fix PoSe connection checks (#4058)
    * llmq: Fix PoSe connection checks
    
    * llmq: Print badConnection if not connected

--- src/llmq/quorums_dkgsession.cpp
+++ src/llmq/quorums_dkgsession.cpp
@@ -472,6 +472,7 @@ void CDKGSession::VerifyConnectionAndMinProtoVersions()
         protoMap.emplace(pnode->verifiedProRegTxHash, pnode->nVersion);
     });
 
+    bool fShouldAllMembersBeConnected = CLLMQUtils::IsAllMembersConnectedEnabled(params.type);
     for (auto& m : members) {
         if (m->dmn->proTxHash == myProTxHash) {
             continue;
@@ -479,8 +480,8 @@ void CDKGSession::VerifyConnectionAndMinProtoVersions()
 
         auto it = protoMap.find(m->dmn->proTxHash);
         if (it == protoMap.end()) {
-            m->badConnection = true;
-            logger.Batch("%s is not connected to us", m->dmn->proTxHash.ToString());
+            m->badConnection = fShouldAllMembersBeConnected;
+            logger.Batch("%s is not connected to us, badConnection=%b", m->dmn->proTxHash.ToString(), m->badConnection);
         } else if (it != protoMap.end() && it->second < MIN_MASTERNODE_PROTO_VERSION) {
             m->badConnection = true;
             logger.Batch("%s does not have min proto version %d (has %d)", m->dmn->proTxHash.ToString(), MIN_MASTERNODE_PROTO_VERSION, it->second);
