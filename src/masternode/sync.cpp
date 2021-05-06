fb9c62856ea29f9071aa7e27fabfd5082fd35d34
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-05-06 06:27:42 +0300

    Initialize `nTime*` members of `CMasternodeSync` with `GetTime()` (#4129)

--- src/masternode/sync.cpp
+++ src/masternode/sync.cpp
@@ -15,6 +15,12 @@
 class CMasternodeSync;
 CMasternodeSync masternodeSync;
 
+CMasternodeSync::CMasternodeSync()
+{
+    nTimeAssetSyncStarted = GetTime();
+    nTimeLastBumped = GetTime();
+}
+
 void CMasternodeSync::Reset(bool fForce, bool fNotifyReset)
 {
     // Avoid resetting the sync process if we just "recently" received a new block
