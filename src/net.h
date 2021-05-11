7bf7971303d970f5c166ce0919fe162ddfed8847
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2021-05-11 20:41:07 +0530

    Merge #16702: supplying and using asmap to improve IP bucketing

--- src/net.h
+++ src/net.h
@@ -177,6 +177,7 @@ public:
         std::vector<std::string> m_specified_outgoing;
         std::vector<std::string> m_added_nodes;
         SocketEventsMode socketEventsMode = SOCKETEVENTS_SELECT;
+        std::vector<bool> m_asmap;
     };
 
     void Init(const Options& connOptions) {
@@ -476,6 +477,8 @@ public:
         Variable intervals will result in privacy decrease.
     */
     int64_t PoissonNextSendInbound(int64_t now, int average_interval_seconds);
+
+    void SetAsmap(std::vector<bool> asmap) { addrman.m_asmap = asmap; }
 
 private:
     struct ListenSocket {
@@ -777,6 +780,7 @@ public:
     CAddress addr;
     // Bind address of our side of the connection
     CAddress addrBind;
+    uint32_t m_mapped_as;
     // In case this is a verified MN, this value is the proTx of the MN
     uint256 verifiedProRegTxHash;
     // In case this is a verified MN, this value is the hashed operator pubkey of the MN
@@ -1119,7 +1123,7 @@ public:
 
     void CloseSocketDisconnect(CConnman* connman);
 
-    void copyStats(CNodeStats &stats);
+    void copyStats(CNodeStats &stats, std::vector<bool> &m_asmap);
 
     ServiceFlags GetLocalServices() const
     {
