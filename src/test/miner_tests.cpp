8d8661ee67436dbf44de92bf5134e7b1a0a3fb3f
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-06-23 11:12:11 +0300

    Fix mempool locks

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -530,6 +530,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     SetMockTime(0);
     mempool.clear();
 
+    LOCK(::mempool.cs);
     TestPackageSelection(chainparams, scriptPubKey, txFirst);
 
     fCheckpointsEnabled = true;
