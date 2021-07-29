46540e7080b15db69cb58fb516fd6d763d5e820e
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-07-29 03:33:51 +0300

    More of 13115: addrman: Add Clang thread safety annotations for variables guarded by CAddrMan.cs

--- src/addrman.h
+++ src/addrman.h
@@ -213,7 +213,7 @@ private:
     int64_t nLastGood GUARDED_BY(cs);
 
     // discriminate entries based on port. Should be false on mainnet/testnet and can be true on devnet/regtest
-    bool discriminatePorts;
+    bool discriminatePorts GUARDED_BY(cs);
 
     //! Holds addrs inserted into tried table that collide with existing entries. Test-before-evict discipline used to resolve these collisions.
     std::set<int> m_tried_collisions;
@@ -277,7 +277,7 @@ protected:
     void SetServices_(const CService &addr, ServiceFlags nServices) EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     //! Get address info for address
-    CAddrInfo GetAddressInfo_(const CService& addr);
+    CAddrInfo GetAddressInfo_(const CService& addr) EXCLUSIVE_LOCKS_REQUIRED(cs);
 
 public:
     //! Serialization versions.
