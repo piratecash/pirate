3e93173243ac18bd91e3bf630ee2b3bde08b7f2b
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-09-01 04:12:41 +0300

    Add missing cs_main locks/checks

--- src/validation.cpp
+++ src/validation.cpp
@@ -2505,6 +2505,7 @@ static void AppendWarning(std::string& res, const std::string& warn)
 
 /** Check warning conditions and do some notifications on new chain tip set. */
 void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainParams) {
+    AssertLockHeld(cs_main);
     // New best block
     mempool.AddTransactionsUpdated(1);
 
@@ -3642,6 +3643,7 @@ static bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationSta
  */
 static bool ContextualCheckBlock(const CBlock& block, CValidationState& state, const Consensus::Params& consensusParams, const CBlockIndex* pindexPrev, bool fCheckProof)
 {
+    AssertLockHeld(cs_main);
     const int nHeight = pindexPrev == nullptr ? 0 : pindexPrev->nHeight + 1;
 
     // Start enforcing BIP113 (Median Time Past) using versionbits logic.
