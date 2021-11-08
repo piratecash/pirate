223eb30ecacb6cb0fbe3cce96da602d2c7476ff6
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-11-09 00:13:24 +0530

    merge bitcoin#8365: Treat high-sigop transactions as larger rather than rejecting them (#4562)

--- src/validation.h
+++ src/validation.h
@@ -121,7 +121,6 @@ static const int64_t MAX_FEE_ESTIMATION_TIP_AGE = 3 * 60 * 60;
 
 /** Default for -permitbaremultisig */
 static const bool DEFAULT_PERMIT_BAREMULTISIG = true;
-static const unsigned int DEFAULT_BYTES_PER_SIGOP = 20;
 static const bool DEFAULT_CHECKPOINTS_ENABLED = true;
 static const bool DEFAULT_TXINDEX = true;
 static const bool DEFAULT_ADDRESSINDEX = false;
@@ -183,7 +182,6 @@ extern bool fIsBareMultisigStd;
  */
 extern bool g_parallel_script_checks;
 extern bool fRequireStandard;
-extern unsigned int nBytesPerSigOp;
 extern bool fCheckBlockIndex;
 extern bool fCheckpointsEnabled;
 extern size_t nCoinCacheUsage;
