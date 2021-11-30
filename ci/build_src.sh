e36fb8a7f3a4442d4fd7e89d3b5416b4207444f8
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-11-30 14:13:18 +0300

    build/ci: Fix --enable-glibc-back-compat and its usage (#4592)
    * build: Fix `--enable-glibc-back-compat`
    
    Compiling on Ubuntu 20.04 results in binaries that can't be run on older systems we still support (e.g. Ubuntu 16.04) and `contrib/devtools/symbol-check.py` complains about it.
    
    Available versions for `log` for example:
    $ objdump -T /lib/x86_64-linux-gnu/libm.so.6 | egrep -w 'log'
    00000000000431b0 g    DF .text	0000000000000039  GLIBC_2.29  log
    0000000000012360 g    DF .text	0000000000000066 (GLIBC_2.2.5) log
    
    (i.e. the default one is 2.29)
    
    This commit fixes the issue by picking the version we support instead of the default one.
    
    Before:
    $ objdump -T dashd | egrep -w 'log'
    0000000000000000      DF *UND*	0000000000000000  GLIBC_2.29  log
    
    After:
    $ objdump -T dashd | egrep -w 'log'
    0000000000000000      DF *UND*	0000000000000000  GLIBC_2.2.5 log
    
    * ci: Add `make check-symbols` to the `build` step
    
    * ci: Do not specify `--enable-glibc-back-compat` for non-release builds
    
    * ci: Set correct LDFLAGS for the release build
    
    * doc: Update docs to mention the need for `LDFLAGS=-static-libstdc++` when compiling for same host but different distro
    
    * ci: Add `--with-sanitizers=undefined` back to `linux64` build

--- ci/build_src.sh
+++ ci/build_src.sh
@@ -51,3 +51,4 @@ cd cosantacore-$BUILD_TARGET
 ./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 
 make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && make $GOAL V=1 ; false )
+make $MAKEJOBS -C src check-symbols
