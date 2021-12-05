bb1502b96ca3e82b2982f531fb11af688728511b
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-12-05 18:45:22 +0530

    merge bitcoin#16701: Replace functions deprecated in Qt 5.13

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -54,7 +54,6 @@
 #include <QDateTime>
 #include <QDebug>
 #include <QDesktopServices>
-#include <QDesktopWidget>
 #include <QDialogButtonBox>
 #include <QDoubleValidator>
 #include <QFileDialog>
@@ -1923,7 +1922,7 @@ qreal calculateIdealFontSize(int width, const QString& text, QFont font, qreal m
     while(font_size >= minPointSize) {
         font.setPointSizeF(font_size);
         QFontMetrics fm(font);
-        if (fm.width(text) < width) {
+        if (TextWidth(fm, text) < width) {
             break;
         }
         font_size -= 0.5;
@@ -1960,6 +1959,15 @@ void PolishProgressDialog(QProgressDialog* dialog)
     dialog->show();
 #else
     Q_UNUSED(dialog);
+#endif
+}
+
+int TextWidth(const QFontMetrics& fm, const QString& text)
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
+    return fm.horizontalAdvance(text);
+#else
+    return fm.width(text);
 #endif
 }
 
