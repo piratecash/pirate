2f71997ae3a179a5d0fc56c924f3064abf231e65
UdjinM6 <UdjinM6@users.noreply.github.com>
2022-01-27 04:02:24 +0300

    12909 followup

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -62,7 +62,6 @@ static const size_t OUTPUT_GROUP_MAX_ENTRIES = 10;
 
 static CCriticalSection cs_wallets;
 static std::vector<std::shared_ptr<CWallet>> vpwallets GUARDED_BY(cs_wallets);
-bool g_wallet_allow_fallback_fee = true; //<! will be defined via chainparams
 
 bool AddWallet(const std::shared_ptr<CWallet>& wallet)
 {
@@ -4029,7 +4028,7 @@ bool CWallet::CreateTransaction(interfaces::Chain::Lock& locked_chain, const std
                     }
                 }
 
-                if (feeCalc.reason == FeeReason::FALLBACK && !g_wallet_allow_fallback_fee) {
+                if (feeCalc.reason == FeeReason::FALLBACK && !m_allow_fallback_fee) {
                     // eventually allow a fallback fee
                     strFailReason = _("Fee estimation failed. Fallbackfee is disabled. Wait a few blocks or enable -fallbackfee.");
                     return false;
