bytzck/BYTZ

Use smaller LLMQs in regtest (#3269)
* Rename LLMQ_5_60 to LLMQ_TEST

* Introduce -llmqtestparams which allows to modify LLMQ_TEST on regtest

Also add support in DashTestFramework

* Use parameters size=3, threshold=2 as default for LLMQ_TEST

And fall back to the old parameters where necessary

* Wait for all contributions, even when one member is lying

Otherwise we might end up continuing too fast, which would fail the DKG.

* Update src/chainparams.cpp

Co-Authored-By: UdjinM6 <UdjinM6@users.noreply.github.com>

Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

test/functional/dip4-coinbasemerkleroots.py
@@ -38,7 +38,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(200)
 
     def run_test(self):

test/functional/llmq-chainlocks.py
@@ -18,7 +18,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(10)
 
     def run_test(self):

test/functional/llmq-dkgerrors.py
@@ -15,7 +15,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, [["-whitelist=127.0.0.1"]] * 4, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(10)
 
     def run_test(self):
@@ -33,18 +33,18 @@ class LLMQDKGErrors(DashTestFramework):
 
         # Lets omit the contribution
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4)
+        qh = self.mine_quorum(expected_contributions=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie in the contribution but provide a correct justification
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
         # Lets lie in the contribution and then omit the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Heal some damage (don't get PoSe banned)
@@ -53,29 +53,26 @@ class LLMQDKGErrors(DashTestFramework):
         # Lets lie in the contribution and then also lie in the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie about another MN
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=1, expected_justifications=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=1, expected_justifications=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets omit 2 premature commitments
+        # Lets omit 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets lie in 2 premature commitments
+        # Lets lie in 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '0')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-lie', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
     def assert_member_valid(self, quorumHash, proTxHash, expectedValid):

piratecash/pirate

test/functional/dip4-coinbasemerkleroots.py
@@ -39,7 +39,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(CosantaTestFramework):
     def set_test_params(self):
-        self.set_cosanta_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_cosanta_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_cosanta_dip8_activation(200)
 
     def run_test(self):

test/functional/llmq-chainlocks.py
@@ -19,7 +19,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(CosantaTestFramework):
     def set_test_params(self):
-        self.set_cosanta_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_cosanta_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_cosanta_dip8_activation(10)
 
     def run_test(self):

test/functional/llmq-dkgerrors.py
@@ -16,7 +16,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(CosantaTestFramework):
     def set_test_params(self):
-        self.set_cosanta_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
+        self.set_cosanta_test_params(4, 3, [["-whitelist=127.0.0.1"]] * 4, fast_dip3_enforcement=True)
         self.set_cosanta_dip8_activation(10)
 
     def run_test(self):
@@ -34,18 +34,18 @@ class LLMQDKGErrors(CosantaTestFramework):
 
         # Lets omit the contribution
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4)
+        qh = self.mine_quorum(expected_contributions=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie in the contribution but provide a correct justification
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
         # Lets lie in the contribution and then omit the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Heal some damage (don't get PoSe banned)
@@ -54,29 +54,26 @@ class LLMQDKGErrors(CosantaTestFramework):
         # Lets lie in the contribution and then also lie in the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie about another MN
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=1, expected_justifications=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=1, expected_justifications=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets omit 2 premature commitments
+        # Lets omit 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets lie in 2 premature commitments
+        # Lets lie in 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '0')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-lie', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
     def assert_member_valid(self, quorumHash, proTxHash, expectedValid):