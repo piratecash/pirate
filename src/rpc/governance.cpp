bytzck/BYTZ

Fixes after 10753

src/rpc/governance.cpp
@@ -1098,7 +1098,7 @@ static const CRPCCommand commands[] =
     { "dash",               "getgovernanceinfo",      &getgovernanceinfo,      {} },
     { "dash",               "getsuperblockbudget",    &getsuperblockbudget,    {"index"} },
     { "dash",               "gobject",                &gobject,                {} },
-    { "dash",               "voteraw",                &voteraw,                {} },
+    { "dash",               "voteraw",                &voteraw,                {"tx_hash","tx_index","gov_hash","signal","outcome","time","sig"} },
 
 };
 

src/rpc/misc.cpp
@@ -1307,7 +1307,7 @@ static const CRPCCommand commands[] =
 
     /* Dash features */
     { "dash",               "mnsync",                 &mnsync,                 {} },
-    { "dash",               "spork",                  &spork,                  {"value"} },
+    { "dash",               "spork",                  &spork,                  {"arg0","value"} },
 
     /* Not shown in help */
     { "hidden",             "setmocktime",            &setmocktime,            {"timestamp"}},

piratecash/pirate

src/rpc/governance.cpp
@@ -1097,7 +1097,7 @@ static const CRPCCommand commands[] =
     { "cosanta",               "getgovernanceinfo",      &getgovernanceinfo,      {} },
     { "cosanta",               "getsuperblockbudget",    &getsuperblockbudget,    {"index"} },
     { "cosanta",               "gobject",                &gobject,                {} },
-    { "cosanta",               "voteraw",                &voteraw,                {} },
+    { "cosanta",               "voteraw",                &voteraw,                {"tx_hash","tx_index","gov_hash","signal","outcome","time","sig"} },
 
 };
 

src/rpc/misc.cpp
@@ -1360,7 +1360,7 @@ static const CRPCCommand commands[] =
 
     /* Cosanta features */
     { "cosanta",               "mnsync",                 &mnsync,                 {} },
-    { "cosanta",               "spork",                  &spork,                  {"value"} },
+    { "cosanta",               "spork",                  &spork,                  {"arg0","value"} },
 
     /* Not shown in help */
     { "hidden",             "setmocktime",            &setmocktime,            {"timestamp"}},