bytzck/BYTZ

Fix thread names to fit into 15 symbols (#3897)

src/bls/bls_worker.cpp
@@ -63,7 +63,7 @@ void CBLSWorker::Start()
     int workerCount = std::thread::hardware_concurrency() / 2;
     workerCount = std::max(std::min(1, workerCount), 4);
     workerPool.resize(workerCount);
-    RenameThreadPool(workerPool, "dash-bls-worker");
+    RenameThreadPool(workerPool, "dash-bls-work");
 }
 
 void CBLSWorker::Stop()

piratecash/pirate

src/bls/bls_worker.cpp
@@ -64,7 +64,7 @@ void CBLSWorker::Start()
     int workerCount = std::thread::hardware_concurrency() / 2;
     workerCount = std::max(std::min(1, workerCount), 4);
     workerPool.resize(workerCount);
-    RenameThreadPool(workerPool, "cosanta-bls-worker");
+    RenameThreadPool(workerPool, "cosa-bls-work");
 }
 
 void CBLSWorker::Stop()