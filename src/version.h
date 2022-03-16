e2b9ba114e503c11e66ce81436e9e3df45224826
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2022-03-16 17:03:17 +0200

    Fix chain params for devnet and allow connections to 70221

--- src/version.h
+++ src/version.h
@@ -12,13 +12,13 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70219;
+static const int PROTOCOL_VERSION = 70222;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70222;
+static const int MIN_PEER_PROTO_VERSION = 70221;
 
 //! minimum proto version of masternode to accept in DKGs
 static const int MIN_MASTERNODE_PROTO_VERSION = 70222;
