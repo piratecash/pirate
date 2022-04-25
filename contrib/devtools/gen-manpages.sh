bytzck/BYTZ

Merge #12308: contrib: Add support for out-of-tree builds in gen-manpages.sh
526e28220a contrib: Add support for out-of-tree builds in gen-manpages.sh (Wladimir J. van der Laan)

Pull request description:

  This adds support for setting the environment variable `BUILDDIR` to point to executables that are outside the source directory.

  E.g. to invoke the tool when the build is in $PWD/build:

  ```bash
  BUILDDIR=$PWD/build contrib/devtools/gen-manpages.sh
  ```

  This avoids having to manually copy the generated manpages after they end up in the build instead of source path, when setting TOPDIR instead.

Tree-SHA512: 8dc6dd7a47a0c014ae7d27f0ac9d86f69238ec6bac8a3007b975bb88c9f37014755c716c5e62604dd91baad2f8a41fd1544cdca3ba4b59bc76602e6593f4a4a7

contrib/devtools/gen-manpages.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
-SRCDIR=${SRCDIR:-$TOPDIR/src}
+BUILDDIR=${BUILDDIR:-$TOPDIR}
+
+BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$SRCDIR/dashd}
-BITCOINCLI=${BITCOINCLI:-$SRCDIR/dash-cli}
-BITCOINTX=${BITCOINTX:-$SRCDIR/dash-tx}
-BITCOINQT=${BITCOINQT:-$SRCDIR/qt/dash-qt}
+BITCOIND=${BITCOIND:-$BINDIR/dashd}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/dash-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/dash-tx}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/dash-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 

piratecash/pirate

contrib/devtools/gen-manpages.sh
@@ -2,13 +2,15 @@
 
 export LC_ALL=C
 TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
-SRCDIR=${SRCDIR:-$TOPDIR/src}
+BUILDDIR=${BUILDDIR:-$TOPDIR}
+
+BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$SRCDIR/cosantad}
-BITCOINCLI=${BITCOINCLI:-$SRCDIR/cosanta-cli}
-BITCOINTX=${BITCOINTX:-$SRCDIR/cosanta-tx}
-BITCOINQT=${BITCOINQT:-$SRCDIR/qt/cosanta-qt}
+BITCOIND=${BITCOIND:-$BINDIR/cosantad}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/cosanta-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/cosanta-tx}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/cosanta-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 