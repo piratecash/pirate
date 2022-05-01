Bitcoin-ABC/bitcoin-abc

Merge #14244: amount: Move CAmount CENT to unit test header
Summary:
fa84723e73 amount: Move CAmount CENT to unit test header (MarcoFalke)

Pull request description:

  `CAmount` is currently not type-safe. Exporting a constant (`CENT`) that is commonly not referred to by that name might be confusing. `CENT` is only used in two places prior to this commit (`ParseMoney` and `MIN_CHANGE`). So replace these with constants relative to `COIN` and move `CENT` to the unit test header.

Tree-SHA512: 5273e96d8664ced6ae211abde2e20bc763e6e99f89404eec02c621f29e1d235e5f9b1ade933743843fae16fc24b643f883deda9221e3d9fd31229d2ab63a914f

Backport of Core [[https://github.com/bitcoin/bitcoin/pull/14244 | PR14244]]

Test Plan:
  ninja check
  ninja check-functional

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D5489

src/util/moneystr.cpp
@@ -45,7 +45,7 @@ bool ParseMoney(const char *pszIn, Amount &nRet) {
     for (; *p; p++) {
         if (*p == '.') {
             p++;
-            Amount nMult = 10 * CENT;
+            Amount nMult = COIN / 10;
             while (isdigit(*p) && (nMult > Amount::zero())) {
                 nUnits += (*p++ - '0') * nMult;
                 nMult /= 10;

piratecash/pirate

src/util/moneystr.cpp
@@ -49,7 +49,7 @@ bool ParseMoney(const char* pszIn, CAmount& nRet)
         if (*p == '.')
         {
             p++;
-            int64_t nMult = CENT*10;
+            int64_t nMult = COIN / 10;
             while (isdigit(*p) && (nMult > 0))
             {
                 nUnits += nMult * (*p++ - '0');