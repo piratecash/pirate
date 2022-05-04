Bitcoin-ABC/bitcoin-abc

Merge #14720: rpc: Correctly name arguments
Summary:
fa0815c300 rpc: Correctly name arguments (Jon Layton)

Pull request description:

  Consistently use the same name to describe arguments in the documentation and add a test that uses the name.

  By splitting it up, the changes are easier to potentially backport and also make review easier when we switch to `RPCHelpMan`.

  The tests should pass with or without the changes in `src`.

  Partly stolen from #14459 (More RPC help description fixes by ch4ot1c)

Tree-SHA512: 1072992b1e93ac41006613523e54a0a8004f529fcb101eb9d74d91474abb0945a5a7539f249905151b904b87448f9efc0cacbd9e052fbe2ea9111e62f3e7249c

Backport of Core [[https://github.com/bitcoin/bitcoin/pull/14720 | PR14720]]
https://github.com/bitcoin/bitcoin/pull/14720/

Test Plan:
  ninja check
  ninja check-functional

  ./bitcoind
  ./bitcoin-cli help waitforblockheight
  ./bitcoin-cli help getblockheader
  ./bitcoin-cli help pruneblockchain
  ./bitcoin-cli help getchaintxstats
  ./bitcoin-cli help scantxoutset
  ./bitcoin-cli help prioritisetransaction
  ./bitcoin-cli help getblocktemplate
  ./bitcoin-cli help addnode
  ./bitcoin-cli help disconnectnode
  ./bitcoin-cli help setban
  ./bitcoin-cli help setnetworkactive
  ./bitcoin-cli help uptime
  ./bitcoin-cli help importprunedfunds
  ./bitcoin-cli help importmulti
  ./bitcoin-cli help getbalance
  ./bitcoin-cli help listtransactions
Verify help text

  ./bitcoin-cli uptime <anything>
Verify this fails with error code -1 and then displays the help message.

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Differential Revision: https://reviews.bitcoinabc.org/D5281

src/wallet/rpcwallet.cpp
@@ -1099,7 +1099,7 @@ static UniValue getbalance(const Config &config,
                        "                     The default is 1 if an account is "
                        "provided or 0 if no account is provided\n")
                  : std::string(
-                       "getbalance ( \"(dummy)\" minconf include_watchonly )\n"
+                       "getbalance ( \"dummy\" minconf include_watchonly )\n"
                        "\nReturns the total available balance.\n"
                        "The available balance is what the wallet considers "
                        "currently spendable, and is\n"
@@ -2206,7 +2206,7 @@ UniValue listtransactions(const Config &config, const JSONRPCRequest &request) {
     std::string help_text{};
     if (!IsDeprecatedRPCEnabled(gArgs, "accounts")) {
         help_text =
-            "listtransactions (dummy count skip include_watchonly)\n"
+            "listtransactions ( \"dummy\" count skip include_watchonly)\n"
             "\nReturns up to 'count' most recent transactions skipping the "
             "first 'from' transactions for account 'account'.\n"
             "Note that the \"account\" argument and \"otheraccount\" return "

piratecash/pirate

src/wallet/rpcwallet.cpp
@@ -926,7 +926,7 @@ static UniValue getbalance(const JSONRPCRequest& request)
             "2. minconf           (numeric, optional) Only include transactions confirmed at least this many times.\n"
             "                     The default is 1 if an account is provided or 0 if no account is provided\n")
             : std::string(
-            "getbalance ( \"(dummy)\" minconf addlocked include_watchonly )\n"
+            "getbalance ( \"dummy\" minconf addlocked include_watchonly )\n"
             "\nReturns the total available balance.\n"
             "The available balance is what the wallet considers currently spendable, and is\n"
             "thus affected by options which limit spendability such as -spendzeroconfchange.\n"