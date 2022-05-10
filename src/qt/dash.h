bitcoin/bitcoin

Merge #11625: Add BitcoinApplication & RPCConsole tests
7e4bd19785 Add BitcoinApplication & RPCConsole tests (Russell Yanofsky)
ca20b65cc0 Move BitcoinApplication to header so it can be tested (Russell Yanofsky)

Pull request description:

  Add test coverage for Qt initialization code & basic RPC console functionality

  Motivation for this change was a bug in #11603 which existing tests failed to catch.

Tree-SHA512: f66546ffc84b8e07679c66a73b265023fbf6a0cb8f24f1606a5fcae2dd3b4dc7b2c6d26c69dedcec53398a26ef17c4d5fb28c055698fa6e45e89aa2995cefe2f

src/qt/dash.h
@@ -1,0 +1,124 @@
+// Copyright (c) 2011-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_DASH_H
+#define BITCOIN_QT_DASH_H
+
+#if defined(HAVE_CONFIG_H)
+#include <config/dash-config.h>
+#endif
+
+#include <QApplication>
+#include <memory>
+#include <vector>
+
+class BitcoinGUI;
+class ClientModel;
+class NetworkStyle;
+class OptionsModel;
+class PaymentServer;
+class WalletModel;
+
+namespace interfaces {
+class Handler;
+class Node;
+} // namespace interfaces
+
+/** Class encapsulating Bitcoin Core startup and shutdown.
+ * Allows running startup and shutdown in a different thread from the UI thread.
+ */
+class BitcoinCore: public QObject
+{
+    Q_OBJECT
+public:
+    explicit BitcoinCore(interfaces::Node& node);
+
+public Q_SLOTS:
+    void initialize();
+    void shutdown();
+    void restart(QStringList args);
+
+Q_SIGNALS:
+    void initializeResult(bool success);
+    void shutdownResult();
+    void runawayException(const QString &message);
+
+private:
+    /// Pass fatal exception message to UI thread
+    void handleRunawayException(const std::exception_ptr e);
+
+    interfaces::Node& m_node;
+};
+
+/** Main Bitcoin application object */
+class BitcoinApplication: public QApplication
+{
+    Q_OBJECT
+public:
+    explicit BitcoinApplication(interfaces::Node& node, int &argc, char **argv);
+    ~BitcoinApplication();
+
+#ifdef ENABLE_WALLET
+    /// Create payment server
+    void createPaymentServer();
+#endif
+    /// parameter interaction/setup based on rules
+    void parameterSetup();
+    /// Create options model
+    void createOptionsModel(bool resetSettings);
+    /// Create main window
+    void createWindow(const NetworkStyle *networkStyle);
+    /// Create splash screen
+    void createSplashScreen(const NetworkStyle *networkStyle);
+    /// Basic initialization, before starting initialization/shutdown thread. Return true on success.
+    bool baseInitialize();
+
+    /// Request core initialization
+    void requestInitialize();
+    /// Request core shutdown
+    void requestShutdown();
+
+    /// Get process return value
+    int getReturnValue() const { return returnValue; }
+
+    /// Get window identifier of QMainWindow (BitcoinGUI)
+    WId getMainWinId() const;
+
+public Q_SLOTS:
+    void initializeResult(bool success);
+    void shutdownResult();
+    /// Handle runaway exceptions. Shows a message box with the problem and quits the program.
+    void handleRunawayException(const QString &message);
+    void addWallet(WalletModel* walletModel);
+    void removeWallet();
+
+Q_SIGNALS:
+    void requestedInitialize();
+    void requestedRestart(QStringList args);
+    void requestedShutdown();
+    void stopThread();
+    void splashFinished();
+    void windowShown(BitcoinGUI* window);
+
+private:
+    QThread *coreThread;
+    interfaces::Node& m_node;
+    OptionsModel *optionsModel;
+    ClientModel *clientModel;
+    BitcoinGUI *window;
+    QTimer *pollShutdownTimer;
+#ifdef ENABLE_WALLET
+    PaymentServer* paymentServer;
+    std::vector<WalletModel*> m_wallet_models;
+    std::unique_ptr<interfaces::Handler> m_handler_load_wallet;
+#endif
+    int returnValue;
+    std::unique_ptr<QWidget> shutdownWindow;
+
+    void startThread();
+};
+
+int GuiMain(int argc, char* argv[]);
+
+#endif // BITCOIN_QT_DASH_H

src/qt/test/apptests.cpp
@@ -1,0 +1,117 @@
+// Copyright (c) 2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/test/apptests.h>
+
+#include <chainparams.h>
+#include <init.h>
+#include <qt/dash.h>
+#include <qt/bitcoingui.h>
+#include <qt/networkstyle.h>
+#include <qt/rpcconsole.h>
+#include <shutdown.h>
+#include <validation.h>
+
+#if defined(HAVE_CONFIG_H)
+#include <config/dash-config.h>
+#endif
+#ifdef ENABLE_WALLET
+#include <wallet/db.h>
+#endif
+
+#include <QAction>
+#include <QEventLoop>
+#include <QLineEdit>
+#include <QScopedPointer>
+#include <QSettings>
+#include <QTest>
+#include <QTextEdit>
+#include <QtGlobal>
+#include <QtTest/QtTestWidgets>
+#include <QtTest/QtTestGui>
+#include <new>
+#include <string>
+#include <univalue.h>
+
+namespace {
+//! Call getblockchaininfo RPC and check first field of JSON output.
+void TestRpcCommand(RPCConsole* console)
+{
+    QEventLoop loop;
+    QTextEdit* messagesWidget = console->findChild<QTextEdit*>("messagesWidget");
+    QObject::connect(messagesWidget, &QTextEdit::textChanged, &loop, &QEventLoop::quit);
+    QLineEdit* lineEdit = console->findChild<QLineEdit*>("lineEdit");
+    QTest::keyClicks(lineEdit, "getblockchaininfo");
+    QTest::keyClick(lineEdit, Qt::Key_Return);
+    loop.exec();
+    QString output = messagesWidget->toPlainText();
+    UniValue value;
+    value.read(output.right(output.size() - output.indexOf("{")).toStdString());
+    QCOMPARE(value["chain"].get_str(), std::string("regtest"));
+}
+} // namespace
+
+//! Entry point for BitcoinApplication tests.
+void AppTests::appTests()
+{
+#ifdef Q_OS_MAC
+    if (QApplication::platformName() == "minimal") {
+        // Disable for mac on "minimal" platform to avoid crashes inside the Qt
+        // framework when it tries to look up unimplemented cocoa functions,
+        // and fails to handle returned nulls
+        // (https://bugreports.qt.io/browse/QTBUG-49686).
+        QWARN("Skipping AppTests on mac build with 'minimal' platform set due to Qt bugs. To run AppTests, invoke "
+              "with 'test_dash-qt -platform cocoa' on mac, or else use a linux or windows build.");
+        return;
+    }
+#endif
+
+    m_app.parameterSetup();
+    GUIUtil::loadFonts();
+    m_app.createOptionsModel(true /* reset settings */);
+    QScopedPointer<const NetworkStyle> style(
+        NetworkStyle::instantiate(QString::fromStdString(Params().NetworkIDString())));
+    m_app.createWindow(style.data());
+    connect(&m_app, &BitcoinApplication::windowShown, this, &AppTests::guiTests);
+    expectCallback("guiTests");
+    QSettings().setValue("fAppearanceSetupDone", true); // skip appearance setup
+    m_app.baseInitialize();
+    m_app.requestInitialize();
+    m_app.exec();
+    m_app.requestShutdown();
+    m_app.exec();
+
+    // Reset global state to avoid interfering with later tests.
+    AbortShutdown();
+    UnloadBlockIndex();
+}
+
+//! Entry point for BitcoinGUI tests.
+void AppTests::guiTests(BitcoinGUI* window)
+{
+    HandleCallback callback{"guiTests", *this};
+    connect(window, &BitcoinGUI::consoleShown, this, &AppTests::consoleTests);
+    expectCallback("consoleTests");
+    QAction* action = window->findChild<QAction*>("openRPCConsoleAction");
+    action->activate(QAction::Trigger);
+}
+
+//! Entry point for RPCConsole tests.
+void AppTests::consoleTests(RPCConsole* console)
+{
+    HandleCallback callback{"consoleTests", *this};
+    TestRpcCommand(console);
+}
+
+//! Destructor to shut down after the last expected callback completes.
+AppTests::HandleCallback::~HandleCallback()
+{
+    auto& callbacks = m_app_tests.m_callbacks;
+    auto it = callbacks.find(m_callback);
+    assert(it != callbacks.end());
+    callbacks.erase(it);
+    if (callbacks.empty()) {
+        m_app_tests.m_app.quit();
+    }
+}

piratecash/pirate

src/qt/dash.h
@@ -1,0 +1,124 @@
+// Copyright (c) 2011-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_DASH_H
+#define BITCOIN_QT_DASH_H
+
+#if defined(HAVE_CONFIG_H)
+#include <config/cosanta-config.h>
+#endif
+
+#include <QApplication>
+#include <memory>
+#include <vector>
+
+class BitcoinGUI;
+class ClientModel;
+class NetworkStyle;
+class OptionsModel;
+class PaymentServer;
+class WalletModel;
+
+namespace interfaces {
+class Handler;
+class Node;
+} // namespace interfaces
+
+/** Class encapsulating Bitcoin Core startup and shutdown.
+ * Allows running startup and shutdown in a different thread from the UI thread.
+ */
+class BitcoinCore: public QObject
+{
+    Q_OBJECT
+public:
+    explicit BitcoinCore(interfaces::Node& node);
+
+public Q_SLOTS:
+    void initialize();
+    void shutdown();
+    void restart(QStringList args);
+
+Q_SIGNALS:
+    void initializeResult(bool success);
+    void shutdownResult();
+    void runawayException(const QString &message);
+
+private:
+    /// Pass fatal exception message to UI thread
+    void handleRunawayException(const std::exception_ptr e);
+
+    interfaces::Node& m_node;
+};
+
+/** Main Bitcoin application object */
+class BitcoinApplication: public QApplication
+{
+    Q_OBJECT
+public:
+    explicit BitcoinApplication(interfaces::Node& node, int &argc, char **argv);
+    ~BitcoinApplication();
+
+#ifdef ENABLE_WALLET
+    /// Create payment server
+    void createPaymentServer();
+#endif
+    /// parameter interaction/setup based on rules
+    void parameterSetup();
+    /// Create options model
+    void createOptionsModel(bool resetSettings);
+    /// Create main window
+    void createWindow(const NetworkStyle *networkStyle);
+    /// Create splash screen
+    void createSplashScreen(const NetworkStyle *networkStyle);
+    /// Basic initialization, before starting initialization/shutdown thread. Return true on success.
+    bool baseInitialize();
+
+    /// Request core initialization
+    void requestInitialize();
+    /// Request core shutdown
+    void requestShutdown();
+
+    /// Get process return value
+    int getReturnValue() const { return returnValue; }
+
+    /// Get window identifier of QMainWindow (BitcoinGUI)
+    WId getMainWinId() const;
+
+public Q_SLOTS:
+    void initializeResult(bool success);
+    void shutdownResult();
+    /// Handle runaway exceptions. Shows a message box with the problem and quits the program.
+    void handleRunawayException(const QString &message);
+    void addWallet(WalletModel* walletModel);
+    void removeWallet();
+
+Q_SIGNALS:
+    void requestedInitialize();
+    void requestedRestart(QStringList args);
+    void requestedShutdown();
+    void stopThread();
+    void splashFinished();
+    void windowShown(BitcoinGUI* window);
+
+private:
+    QThread *coreThread;
+    interfaces::Node& m_node;
+    OptionsModel *optionsModel;
+    ClientModel *clientModel;
+    BitcoinGUI *window;
+    QTimer *pollShutdownTimer;
+#ifdef ENABLE_WALLET
+    PaymentServer* paymentServer;
+    std::vector<WalletModel*> m_wallet_models;
+    std::unique_ptr<interfaces::Handler> m_handler_load_wallet;
+#endif
+    int returnValue;
+    std::unique_ptr<QWidget> shutdownWindow;
+
+    void startThread();
+};
+
+int GuiMain(int argc, char* argv[]);
+
+#endif // BITCOIN_QT_DASH_H

src/qt/test/apptests.cpp
@@ -1,0 +1,117 @@
+// Copyright (c) 2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/test/apptests.h>
+
+#include <chainparams.h>
+#include <init.h>
+#include <qt/dash.h>
+#include <qt/bitcoingui.h>
+#include <qt/networkstyle.h>
+#include <qt/rpcconsole.h>
+#include <shutdown.h>
+#include <validation.h>
+
+#if defined(HAVE_CONFIG_H)
+#include <config/cosanta-config.h>
+#endif
+#ifdef ENABLE_WALLET
+#include <wallet/db.h>
+#endif
+
+#include <QAction>
+#include <QEventLoop>
+#include <QLineEdit>
+#include <QScopedPointer>
+#include <QSettings>
+#include <QTest>
+#include <QTextEdit>
+#include <QtGlobal>
+#include <QtTest/QtTestWidgets>
+#include <QtTest/QtTestGui>
+#include <new>
+#include <string>
+#include <univalue.h>
+
+namespace {
+//! Call getblockchaininfo RPC and check first field of JSON output.
+void TestRpcCommand(RPCConsole* console)
+{
+    QEventLoop loop;
+    QTextEdit* messagesWidget = console->findChild<QTextEdit*>("messagesWidget");
+    QObject::connect(messagesWidget, &QTextEdit::textChanged, &loop, &QEventLoop::quit);
+    QLineEdit* lineEdit = console->findChild<QLineEdit*>("lineEdit");
+    QTest::keyClicks(lineEdit, "getblockchaininfo");
+    QTest::keyClick(lineEdit, Qt::Key_Return);
+    loop.exec();
+    QString output = messagesWidget->toPlainText();
+    UniValue value;
+    value.read(output.right(output.size() - output.indexOf("{")).toStdString());
+    QCOMPARE(value["chain"].get_str(), std::string("regtest"));
+}
+} // namespace
+
+//! Entry point for BitcoinApplication tests.
+void AppTests::appTests()
+{
+#ifdef Q_OS_MAC
+    if (QApplication::platformName() == "minimal") {
+        // Disable for mac on "minimal" platform to avoid crashes inside the Qt
+        // framework when it tries to look up unimplemented cocoa functions,
+        // and fails to handle returned nulls
+        // (https://bugreports.qt.io/browse/QTBUG-49686).
+        QWARN("Skipping AppTests on mac build with 'minimal' platform set due to Qt bugs. To run AppTests, invoke "
+              "with 'test_dash-qt -platform cocoa' on mac, or else use a linux or windows build.");
+        return;
+    }
+#endif
+
+    m_app.parameterSetup();
+    GUIUtil::loadFonts();
+    m_app.createOptionsModel(true /* reset settings */);
+    QScopedPointer<const NetworkStyle> style(
+        NetworkStyle::instantiate(QString::fromStdString(Params().NetworkIDString())));
+    m_app.createWindow(style.data());
+    connect(&m_app, &BitcoinApplication::windowShown, this, &AppTests::guiTests);
+    expectCallback("guiTests");
+    QSettings().setValue("fAppearanceSetupDone", true); // skip appearance setup
+    m_app.baseInitialize();
+    m_app.requestInitialize();
+    m_app.exec();
+    m_app.requestShutdown();
+    m_app.exec();
+
+    // Reset global state to avoid interfering with later tests.
+    AbortShutdown();
+    UnloadBlockIndex();
+}
+
+//! Entry point for BitcoinGUI tests.
+void AppTests::guiTests(BitcoinGUI* window)
+{
+    HandleCallback callback{"guiTests", *this};
+    connect(window, &BitcoinGUI::consoleShown, this, &AppTests::consoleTests);
+    expectCallback("consoleTests");
+    QAction* action = window->findChild<QAction*>("openRPCConsoleAction");
+    action->activate(QAction::Trigger);
+}
+
+//! Entry point for RPCConsole tests.
+void AppTests::consoleTests(RPCConsole* console)
+{
+    HandleCallback callback{"consoleTests", *this};
+    TestRpcCommand(console);
+}
+
+//! Destructor to shut down after the last expected callback completes.
+AppTests::HandleCallback::~HandleCallback()
+{
+    auto& callbacks = m_app_tests.m_callbacks;
+    auto it = callbacks.find(m_callback);
+    assert(it != callbacks.end());
+    callbacks.erase(it);
+    if (callbacks.empty()) {
+        m_app_tests.m_app.quit();
+    }
+}