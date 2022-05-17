499eefd1aa63d5b031bf0111928eb8e6fc9b9fc8
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2022-05-17 14:52:28 +0300

    LLMQ connection retry timeout from chainparams

--- src/net.cpp
+++ src/net.cpp
@@ -2599,7 +2599,7 @@ void CConnman::ThreadOpenMasternodeConnections()
 
                     int64_t lastAttempt = mmetaman.GetMetaInfo(dmn->proTxHash)->GetLastOutboundAttempt();
                     // back off trying connecting to an address if we already tried recently
-                    if (nANow - lastAttempt < 60) {
+                    if (nANow - lastAttempt < chainParams.LLMQConnectionRetryTimeout()) {
                         continue;
                     }
                     pending.emplace_back(dmn);
