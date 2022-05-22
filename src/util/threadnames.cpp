1d86eecf6353f5d7f4cddd917e2f8647bf389cc5
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2022-05-22 12:48:01 +0300

    Make thread names shorter

--- src/util/threadnames.cpp
+++ src/util/threadnames.cpp
@@ -57,6 +57,6 @@ static void SetInternalName(std::string name) { }
 
 void util::ThreadRename(std::string&& name)
 {
-    SetThreadName(("cosa-" + name).c_str());
+    SetThreadName(("c-" + name).c_str());
     SetInternalName(std::move(name));
 }
